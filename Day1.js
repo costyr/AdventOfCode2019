3666a0f1419e49f2520078cbeede33e9c4f30ff4
Costi Rada <costi.rada@hotmail.com>
2019-12-01 08:09:27 +0200

    First day problem

---
+++ Day1.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+var rawDay1Input = fs.readFileSync('./Day1Input.txt');
+
+var day1Input = rawDay1Input.toString().split('\r\n');
+
+var totalFuel = 0;
+for (i = 0; i < day1Input.length; i++) {
+  let moduleFuel = Math.floor(parseInt(day1Input[i], 10) / 3) - 2;
+
+  if (moduleFuel > 0)
+    totalFuel += moduleFuel;
+
+  while (moduleFuel > 0)
+  {
+    moduleFuel = Math.floor(moduleFuel / 3) - 2;
+    if (moduleFuel > 0)
+      totalFuel += moduleFuel;
+  }
+}
+
+console.log("Total Fuel: " + totalFuel);

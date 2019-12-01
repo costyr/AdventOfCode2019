57c23c5585850112d7f2ae39f7f00e5fe003eaaf
Costi Rada <costi.rada@hotmail.com>
2019-12-01 10:27:28 +0200

    Print both solutions

--- Day1.js
+++ Day1.js
@@ -5,6 +5,7 @@ var rawDay1Input = fs.readFileSync('./Day1Input.txt');
 var day1Input = rawDay1Input.toString().split('\r\n');
 
 var totalFuel = 0;
+var totalWithFuel = 0;
 for (i = 0; i < day1Input.length; i++) {
   let moduleFuel = Math.floor(parseInt(day1Input[i], 10) / 3) - 2;
 
@@ -15,8 +16,9 @@ for (i = 0; i < day1Input.length; i++) {
   {
     moduleFuel = Math.floor(moduleFuel / 3) - 2;
     if (moduleFuel > 0)
-      totalFuel += moduleFuel;
+      totalWithFuel += moduleFuel;
   }
 }
 
-console.log("Total Fuel: " + totalFuel);
+console.log("Total fuel: " + totalFuel);
+console.log("Total with fuel: " + (totalFuel + totalWithFuel));

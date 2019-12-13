743c9f0dc9b061896686e01045f5999509ec9784
Costi <costyr@gmail.com>
2019-12-13 17:07:08 +0200

    Day 12 second problem solution

--- Day12.js
+++ Day12.js
@@ -1,6 +1,7 @@
 const util = require('./Util.js');
+const { lcm } = require('mathjs');
 
-var moons = util.MapInput("./Day12TestInput2.txt", ParseMoons, "\r\n");
+var moons = util.MapInput("./Day12Input.txt", ParseMoons, "\r\n");
 
 var initialMoonsPos = util.CopyObject(moons);
 
@@ -36,66 +37,80 @@ function ComputeTotalEnergy(aMoons)
   return total;
 }
 
-function IsSamePos(aMoonPos1, aMoonPos2) 
+function IsSamePos(aMoonPos1, aMoonPos2, aScanX, aScanY, aScanZ) 
 {
-  if (/*(aMoonPos1.pos.x == aMoonPos2.pos.x) && 
-      (aMoonPos1.pos.y == aMoonPos2.pos.y) &&
-      (aMoonPos1.pos.z == aMoonPos2.pos.z) &&*/
-      (aMoonPos1.vel.x == aMoonPos2.vel.x) && 
-      (aMoonPos1.vel.y == aMoonPos2.vel.y) &&
-      (aMoonPos1.vel.z == aMoonPos2.vel.z))
+  if (((aScanX && (aMoonPos1.pos.x == aMoonPos2.pos.x)) || !aScanX) && 
+      ((aScanY && (aMoonPos1.pos.y == aMoonPos2.pos.y)) || !aScanY) &&
+      ((aScanZ && (aMoonPos1.pos.z == aMoonPos2.pos.z)) || !aScanZ))
     return true;
   return false;
 }
 
-function MoonsInInitialPos(aInitialMoonsPos, aMoons) 
+function IsSameVel(aMoonPos1, aMoonPos2, aScanX, aScanY, aScanZ) 
 {
-  for (let i = 0; i < aMoons.length; i++)
-    if (!IsSamePos(aInitialMoonsPos[i], aMoons[i]))
-      return false;
-  return true;
+  if (((aScanX && (aMoonPos1.vel.x == aMoonPos2.vel.x)) || !aScanX) &&
+      ((aScanY && (aMoonPos1.vel.y == aMoonPos2.vel.y)) || !aScanY) &&
+      ((aScanZ && (aMoonPos1.vel.z == aMoonPos2.vel.z)) || !aScanZ))
+    return true;
+  return false;
 }
 
-
-function SimulateMotion(aMoons, aStepsCount, aAcceleration) 
+function MoonsInInitialPos(aInitialMoonsPos, aMoons, aScanX, aScanY, aScanZ) 
 {
+  let samePos = 0;
+  let sameVel = 0;
+  for (let i = 0; i < aMoons.length; i++) 
+  {
+    if (IsSamePos(aInitialMoonsPos[i], aMoons[i], aScanX, aScanY, aScanZ))
+      samePos++;
+    if (IsSameVel(aInitialMoonsPos[i], aMoons[i], aScanX, aScanY, aScanZ))
+      sameVel++;
+  }
+
+  return (samePos == 4) && (sameVel == 4);
+}
+
+function SimulateMotion(aMoons, aStepsCount, aComputeEnergy) 
+{
+  let freqX = 0;
+  let freqY = 0;
+  let freqZ = 0;
   for (let step = 0; step < aStepsCount; step++) 
   {
-    //console.log(step);
     for (let i = 0; i < aMoons.length; i++) 
       for (let j = i + 1; j < aMoons.length; j++) 
       {
         if (aMoons[i].pos.x < aMoons[j].pos.x)
         {
-          aMoons[i].vel.x += aAcceleration;
-          aMoons[j].vel.x += -aAcceleration;
+          aMoons[i].vel.x += 1;
+          aMoons[j].vel.x += -1;
         }
         else if (aMoons[i].pos.x > aMoons[j].pos.x) 
         {
-          aMoons[i].vel.x += -aAcceleration;
-          aMoons[j].vel.x += aAcceleration;
+          aMoons[i].vel.x += -1;
+          aMoons[j].vel.x += 1;
         }
 
         if (aMoons[i].pos.y < aMoons[j].pos.y)
         {
-          aMoons[i].vel.y += aAcceleration;
-          aMoons[j].vel.y += -aAcceleration;
+          aMoons[i].vel.y += 1;
+          aMoons[j].vel.y += -1;
         }
         else if (aMoons[i].pos.y > aMoons[j].pos.y) 
         {
-          aMoons[i].vel.y += -aAcceleration;
-          aMoons[j].vel.y += aAcceleration;
+          aMoons[i].vel.y += -1;
+          aMoons[j].vel.y += 1;
         }
 
         if (aMoons[i].pos.z < aMoons[j].pos.z)
         {
-          aMoons[i].vel.z += aAcceleration;
-          aMoons[j].vel.z += -aAcceleration;
+          aMoons[i].vel.z += 1;
+          aMoons[j].vel.z += -1;
         }
         else if (aMoons[i].pos.z > aMoons[j].pos.z) 
         {
-          aMoons[i].vel.z += -aAcceleration;
-          aMoons[j].vel.z += aAcceleration;
+          aMoons[i].vel.z += -1;
+          aMoons[j].vel.z += 1;
         }
       }
 
@@ -105,20 +120,29 @@ function SimulateMotion(aMoons, aStepsCount, aAcceleration)
       moon.pos.x += moon.vel.x;
       moon.pos.y += moon.vel.y;
       moon.pos.z += moon.vel.z;
-
-      //console.log(step + "-->" + JSON.stringify(moon));
     }
 
-    if (MoonsInInitialPos(initialMoonsPos, aMoons)) {
-      console.log("Moons are in initial position:" + step);
-      return;
-    }
+    let mX = MoonsInInitialPos(initialMoonsPos, aMoons, true, false, false);
+    if (mX && freqX == 0) 
+      freqX = step + 1;
 
-    //console.log();
+    let mY = MoonsInInitialPos(initialMoonsPos, aMoons, false, true, false);
+    if (mY && freqY == 0) 
+      freqY= step + 1;
+
+    let mZ = MoonsInInitialPos(initialMoonsPos, aMoons, false, false, true);
+    if (mZ && freqZ == 0) 
+      freqZ= step + 1;
+
+    if (freqX > 0 && freqY > 0 && freqZ > 0)
+      break;
   }
 
-  return ComputeTotalEnergy(aMoons);
+  if (aComputeEnergy)
+    return ComputeTotalEnergy(aMoons);
+  else 
+    return lcm(freqX, freqY, freqZ);
 }
 
-console.log(moons);
-console.log(SimulateMotion(moons, 10000000000, 3));
+console.log(SimulateMotion(moons, 1000, true));
+console.log(SimulateMotion(util.CopyObject(initialMoonsPos), 1000000000, false));

6598dafe1a93fe1ff3e52a5f139bd5a9ceaefc0a
Costi Rada <costi.rada@caphyon.com>
2020-01-16 14:27:32 +0200

    Day 21 second problem solution

--- Day21.js
+++ Day21.js
@@ -31,61 +31,39 @@ function ConvertScriptInstructionsToAscii(aInstructions) {
 
 var inst = util.MapInput('./Day21Input.txt', util.ParseInt, ',');
 
-let script = [ 
-  "NOT A J",
-  "NOT B T",
-  "OR T J",
-  "NOT C T",
-  "OR T J",
-  "AND D J",
-  "RUN" ]; 
-
-// (!A || !B || !C || !D || !E || !F || !G || !H) && I
-let script2 = [ 
-  "NOT A J",
-  "NOT B T",
-  "OR T J",
-  "NOT C T",
-  "OR T J",
-  "NOT D T",
-  "OR T J",
-  "NOT E T",
-  "OR T J",
-  "NOT F T",
-  "OR T J",
-  "NOT G T",
-  "OR T J",
-  "NOT H J",
-  "OR T J",
-  "AND I J",
-  "RUN" ]; 
-
-  let script3 = [ 
-    "NOT A J",
-    "AND I J",
-    "OR T J",
-    "NOT C T",
-    "OR T J",
-    "NOT D T",
-    "OR T J",
+let scripts = [
+  // (!A || !B || !C) && D
+  [ "NOT A J",
+   "NOT B T",
+   "OR T J",
+   "NOT C T",
+   "OR T J",
+   "AND D J",
+   "WALK" ],
+  // (!(A && B && C) && D) && (E || H)
+  [ "NOT A T",
+    "NOT T T",
+    "AND B T",
+    "AND C T",
+    "NOT T J",
+    "AND D J",
     "NOT E T",
-    "OR T J",
-    "NOT F T",
-    "OR T J",
-    "NOT G T",
-    "OR T J",
-    "NOT H J",
-    "OR T J",
-    "AND I J",
-    "RUN" ]; 
+    "NOT T T",
+    "OR H T",
+    "AND T J",
+    "RUN" ]
+]; 
 
-var rawInput = ConvertScriptInstructionsToAscii(script2);
+for (let i = 0; i < scripts.length; i++) 
+{
+  let rawInput = ConvertScriptInstructionsToAscii(scripts[i]);
 
-let input = new intcodeComputer.IntcodeIOStream(rawInput);
-let output = new intcodeComputer.IntcodeIOStream([]);
+  let input = new intcodeComputer.IntcodeIOStream(rawInput);
+  let output = new intcodeComputer.IntcodeIOStream([]);
 
-var prog = new intcodeComputer.IntcodeProgram(inst, input, output);
+  let prog = new intcodeComputer.IntcodeProgram(inst, input, output);
 
-prog.Run();
+  prog.Run();
 
-console.log(RenderOutput(output.Get()));
+  console.log(RenderOutput(output.Get()));
+}

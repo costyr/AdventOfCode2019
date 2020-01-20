1507fde997d04b8f3dfe9f5a2ee69bc404a8c63c
Costi Rada <costi.rada@caphyon.com>
2020-01-20 08:40:06 +0200

    Code refactoring

--- Day22.js
+++ Day22.js
@@ -57,6 +57,12 @@ function DealWithNIncrement(aCardsNumber, aCardPos, aIncrement) {
   return newCardPos;
 }
 
+function ModularDivide(aBase, aExponent, aModular) {
+  let inv = bigInt(aExponent).modInv(aModular);
+  let c = aBase.multiply(inv);
+  return c.mod(aModular);
+}
+
 function DealCards(aOps, aCardsNumber, aCardPos) {
 
   let cardPos = aCardPos;
@@ -75,73 +81,34 @@ function DealCards(aOps, aCardsNumber, aCardPos) {
       console.log("Invalid deal operation!");
   }
 
-  console.log(cardPos);
-
   return cardPos;
 }
 
 function DealBig(aOps, aCardsNumber, aCardPos, aDealTimes) {
-  let cardPos = aCardPos;
-  for (let i = 0; i < aDealTimes; i++) 
-  {
-    let a = DealCards(aOps, aCardsNumber, 2020);
-    let b = DealCards(aOps, aCardsNumber, 2021);
-    let c = DealCards(aOps, aCardsNumber, 2022);
-    let d = DealCards(aOps, aCardsNumber, 2023);
-    let e = DealCards(aOps, aCardsNumber, 2024);
-    let f = DealCards(aOps, aCardsNumber, 2025);
+  let firstCardPos = DealCards(aOps, aCardsNumber, 0);
+  let secondCardPos = DealCards(aOps, aCardsNumber, 1);
+  let thirdCardPos = DealCards(aOps, aCardsNumber, 2);
 
-    console.log("-------");
-    console.log(b - a);
-    console.log(c - b);
-    console.log(d - c);
-    console.log(e - d);
-    console.log(f - e);
-    console.log("-------");
+  let b = firstCardPos;
+  let a = thirdCardPos - secondCardPos;
 
-    cardPos = DealCards(aOps, aCardsNumber, cardPos);
-  }
+  let bigA = bigInt(a).modPow(aDealTimes, aCardsNumber);
 
-  //console.log(cardPos);
-}
+  let bigB = bigInt(b).multiply(ModularDivide(bigA.subtract(1), a - 1, aCardsNumber)).mod(aCardsNumber);
 
-function ModularDivide(a, b, n) {
-  let inv = bigInt(b).modInv(n);
-  let c = a.multiply(inv);
-  return c.mod(n);
+  let tt = aCardPos - bigB;
+  if (tt < 0)
+    tt += aCardsNumber;
+
+  let cardNumber = ModularDivide(bigInt(tt).mod(aCardsNumber), bigA, aCardsNumber).mod(aCardsNumber);
+
+  return cardNumber;
 }
 
 var ops = util.MapInput('./Day22Input.txt', ParseLine, "\r\n");
 
 console.log(ops);
 
-let x = DealCards(ops, 119315717514047, 0);
-let y = DealCards(ops, 119315717514047, 1);
-let z = DealCards(ops, 119315717514047, 2);
-let t = DealCards(ops, 119315717514047, 100);
+console.log(DealCards(ops, 10007, 2019));
 
-let u = (2019 * 2183 + 2129) % 10007;
-console.log(u);
-
-let ty = (100 * 40286879916729 + 37260864847148) % 119315717514047;
-
-console.log(ty);
-
-//DealCards(ops, 119315717514047, 2020);
-
-//DealBig(ops, 10007/*19315717514047*/, 2020, 100/*101741582076661*/);
-
-let ff = bigInt(40286879916729).modPow(101741582076661, 119315717514047);
-
-let gg = bigInt(37260864847148).multiply(modularDivide(bigInt(40286879916729).modPow(101741582076661, 119315717514047).subtract(1), 40286879916728, 119315717514047)).mod(119315717514047);
-
-let tt = 2020 - gg;
-tt += 119315717514047;
-
-let rr = bigInt(tt).mod(119315717514047);
-
-let xx = modularDivide(rr, ff, 119315717514047).mod(119315717514047);
-
-console.log(ff.toString());
-console.log(gg.toString());
-console.log(xx.toString());
+console.log(DealBig(ops, 119315717514047, 2020, 101741582076661).toString());
